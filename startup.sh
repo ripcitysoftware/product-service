121c77e5c06de3ded0d04142e1dbd69159ac094f
Chris Maki <chris.maki@ripcitysoftware.com>
2019-07-06 09:26:50 -0700

    add documentation to files

--- startup.sh
+++ startup.sh
@@ -4,6 +4,8 @@ JAVA_APP_JAR=${JAVA_APP_JAR:-app.jar}
 JAVA_APP_PATH=${JAVA_APP_PATH:-/app}
 JAVA_APP_JAR_PATH="${JAVA_APP_PATH}/${JAVA_APP_JAR}"
 Xmx="${Xmx:-2048M}"
+# we load the config from the JAR and then we load config from /app/config which will override
+# application.properties
 CONFIG_LOCATION="classpath:/application.properties,/app/conf/"
 SPRING_CONFIG="--spring.config.location=${CONFIG_LOCATION}"
 
